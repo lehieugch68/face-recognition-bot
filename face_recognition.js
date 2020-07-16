0a0b759a0040f87d676ebfbff1f4fd8d40d0710c
Lê Hiếu <51288927+lehieugch68@users.noreply.github.com>
2020-07-16 23:44:39 +0700

    Update face_recognition.js

--- face_recognition.js
+++ face_recognition.js
@@ -3,37 +3,47 @@ const faceapi = require("face-api.js");
 const canvas = require("canvas");
 const { Canvas, Image, ImageData } = canvas; 
 faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
-//faceapi.tf.getBackend();
+faceapi.tf.getBackend();
 const MODEL_URL = "./models";
 
-const expressionTrans = {"Bình thường": "neutral", "Vui mừng": "happy", "Buồn bã": "sad", "Tức giận": "angry", "Sợ hãi": "fearful", "Ghê tởm": "disgusted", "Bất ngờ": "surprised"};
-const genderTrans = {"Nam": "male", "Nữ": "female"};
+const expressionName = {"Bình thường": "neutral", "Vui mừng": "happy", "Buồn bã": "sad", "Tức giận": "angry", "Sợ hãi": "fearful", "Ghê tởm": "disgusted", "Bất ngờ": "surprised"};
+const genderName = {"Nam": "male", "Nữ": "female"};
 
-process.on('message', async function(msg) {
-	await faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_URL); 
-	//await faceapi.nets.tinyFaceDetector.loadFromDisk(MODEL_URL);
-	await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL);
-	await faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_URL);
-	await faceapi.nets.faceExpressionNet.loadFromDisk(MODEL_URL);
-	await faceapi.nets.ageGenderNet.loadFromDisk(MODEL_URL);
-	let url = msg;
-	try {
-		let img = await canvas.loadImage(url);
-		let results = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceExpressions().withAgeAndGender();
-		const out = faceapi.createCanvasFromMedia(img);
-		const displaySize = { width: img.width, height: img.height }; 
-		faceapi.matchDimensions(img, displaySize);
-		const resizedDetections = faceapi.resizeResults(results, displaySize);
-		faceapi.draw.drawDetections(out, resizedDetections);
-		let gender = Object.keys(genderTrans).find(key => genderTrans[key] === resizedDetections.gender);
-		let age = Math.abs(Math.round(resizedDetections.age));
-		let expressions = resizedDetections['expressions'];
-		let max = Math.max.apply(null, Object.values(expressions));
-		let index = Object.keys(expressions).find(key => expressions[key] === max);
-		let expression = Object.keys(expressionTrans).find(key => expressionTrans[key] === index);
-		let analysisResult = `\`\`\`Giới tính: ${gender}\nTuổi: ${age}\nCảm xúc: ${expression}\`\`\``;
-		return process.send({error: false, result: analysisResult, buffer: out.toBuffer()});
-	} catch (err) {
-		return process.send({error: true, errmessage: err.message});
-	}
+Promise.all([
+	faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_URL),
+	faceapi.nets.tinyFaceDetector.loadFromDisk(MODEL_URL),
+	//faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL),
+	faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_URL),
+	faceapi.nets.faceExpressionNet.loadFromDisk(MODEL_URL),
+	faceapi.nets.ageGenderNet.loadFromDisk(MODEL_URL),
+]).then(() => {
+	process.on('message', (msg) => {
+		analysis(msg.url).then(result => { process.send({content: result, user: msg.user, channel: msg.channel}); }).catch(err => { process.send({content: err, user: msg.user, channel: msg.channel}) });
+	})
 })
+
+let analysis = (url) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			let img = await canvas.loadImage(url);
+			let results = await faceapi.detectSingleFace(img, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withAgeAndGender();
+			if (!results) return reject({error: true, message: "Không tìm thấy khuôn mặt nào cả!"});
+			const out = faceapi.createCanvasFromMedia(img);
+			const displaySize = { width: img.width, height: img.height }; 
+			faceapi.matchDimensions(img, displaySize);
+			const resizedDetections = faceapi.resizeResults(results, displaySize);
+			faceapi.draw.drawDetections(out, resizedDetections);
+			const box = resizedDetections.detection.box;
+			let gender = Object.keys(genderName).find(key => genderName[key] === resizedDetections.gender);
+			let age = Math.abs(Math.round(resizedDetections.age));
+			let expressions = resizedDetections['expressions'];
+			let max = Math.max.apply(null, Object.values(expressions));
+			let index = Object.keys(expressions).find(key => expressions[key] === max);
+			let expression = Object.keys(expressionName).find(key => expressionName[key] === index);
+			let result = `\`\`\`Giới tính: ${gender}\nTuổi: ${age}\nCảm xúc: ${expression}\`\`\``;
+			return resolve({error: false, result: result, buffer: out.toBuffer()});
+		} catch (err) {
+			return reject(({error: true, message: err.message}));
+		}
+	})
+}
